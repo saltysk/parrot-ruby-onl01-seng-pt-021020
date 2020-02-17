47849b68926852ec9e0839c27a23e5c5d054a4ab
Leo Kim <sotysk@gmail.com>
2020-02-17 21:11:00 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,4 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(phrase="Squawk!")
+  puts phrase
+  phrase #return value is always the last line of code
+end
